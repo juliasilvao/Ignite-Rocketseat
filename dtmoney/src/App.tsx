f564c2ff36acbfe74b4c54400074a249ae1d9b02
Julia Silva <juliasilva@unimedguara.local>
2021-04-29 18:01:45 -0300

    context

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
 import Modal from "react-modal";
+
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
-
+import { TransactionsProvider } from "./TransactionsContext";
 import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement('#root')
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
-
+  
   function handleOpenNewTransactionModal() {
     setIsNewTransactionModalOpen(true)
   }
@@ -20,7 +21,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 
       <Dashboard />
@@ -31,6 +32,6 @@ export function App() {
       />
 
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
--- dtmoney/src/index.tsx
+++ dtmoney/src/index.tsx
@@ -1,24 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Lanche McDonald's",
+          value: 35.00,
+          category: "Comida",
+          type: "withdraw",
+          createdAt: new Date('2021-04-29 12:30:00')
+        },
+        {
+          id: 2,
+          title: "Notebook",
+          value: 1500.00,
+          category: "Vendas",
+          type: "deposit",
+          createdAt: new Date('2021-02-15 15:00:00')
+        },
+        {
+          id: 3,
+          title: "Desenvolvimento de Site",
+          value: 5000.00,
+          category: "Freelance",
+          type: "deposit",
+          createdAt: new Date('2021-02-19 19:10:00')
+        },
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api'
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: '04/02/2021'
-        }
-      ]
+      return this.schema.all('transaction')
     })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
+    })
+
   }
 })
 
