5ebe3e1d0f1253d0319221adf618cefe7ca1caa8
JULIA DE OLIVEIRA SILVA <julia.silva18@fatec.sp.gov.br>
2021-04-29 01:29:46 -0300

    criação modal

--- dtmoney/src/components/TransactionsTable/index.tsx
+++ dtmoney/src/components/TransactionsTable/index.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
+  useEffect(() => {
+    api.get('transactions')
+      .then(response => console.log(response.data))
+  }, [])
+
   return (
     <Container>
       <table>
@@ -12,7 +19,6 @@ export function TransactionsTable() {
             <th>Data</th>
           </tr>
         </thead>
-
         <tbody>
           <tr>
             <td>Desenvolvimento de website</td>
